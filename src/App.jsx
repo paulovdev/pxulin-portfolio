513b69f90bc3f9b831c663258ff210a49f9d67fe
pxulin <fudevil123@gmail.com>
2023-07-01 13:44:37 -0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,12 @@ import React from 'react'
 import './styles.css'
 import './global.css'
 import Router from './Router';
-import { Cursor } from 'custom-pointer-react'
 const App = () => {
+
   return (
     <>
       <div className="grid-layout">
         <div className="principal">
-          <Cursor
-            showRing={true}
-            color="var(--text-color)"
-            ringSize={50}
-            cursorSize={10}
-            ringBorder={2}
-          />
           <Router />
 
         </div>
